19b636ce6ad7761f89c46387c132ae5fc40f7f06
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-12 16:11:24 -0300

    Inicialização do versionamento do projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/sboot_course_ms/MicrosservicoDeCursoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_course_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoDeCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoDeCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/sboot_course_ms/MicrosservicoDeCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_course_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoDeCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
